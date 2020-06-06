d412670f11969f7f3ca71eaab4fc33262c45879a
savio591 <savio591@hotmail.com>
2020-06-06 20:30:01 -0300

    tuplas added

---
+++ teste_tuplash.py
@@ -0,0 +1,20 @@
+""""
+Tuplas(tuples)
+
+    São bastante parecidas com as listas, porém, contém duas variáveis em uma só.
+    Diferente de listas, que são uma sequência de valores em uma variável.
+    --As tuplas são imutáveis. Apenas são adicionadas novas variáveis.
+"""
+# exemplo de lista
+lista = [1, 5, 8, 34, 23]
+print(lista)
+
+lista.sort()
+print(lista)
+
+# adicionando valor na lista
+lista.append(44)
+print(lista)
+
+# substituindo valor na lista
+lista.append()
